a77aefaa0ccbb75322872222507075ea007ded68
ronmaya <105212794+ronmaya@users.noreply.github.com>
2022-08-05 16:33:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+// Exercise 2, addition
+
+const x = 9;
+const y = 48;
+
+const xy = x + y;
+console.log(xy);
+
+// Substraction
+const p = 23;
+const l = 74;
+
+const pl = p - l;
+console.log(pl);
+
+// Multiplication
+const r = 6 ;
+const q = 73;
+
+const rq = r * q;
+console.log(rq);
+
+// Division
+const f = 93;
+const d = 21;
+
+const fd = f / d;
+console.log(fd);
+
+// Modules
+const k = 65;
+const c = 4;
+
+const kc = k % c;
+console.log(kc);
+
+// exponent 
+const w =5;
+const z = 9;
+
+
+
+// Exercise 3 
+
+const friends=['John', 'Peter', 'Brian','Paul', 'juma'];
+
+//  1
+console.log(friends[4])
+
+// 2
+
+console.log(friends[0])
+
+// Exercise 4
+
+const car = {
+    make: "Ford",
+    model: "Mustang",
+    year : 1996,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 200
+
+}
+
+console.log(car.make + " is a new brand Vehicle in "+ car.year + ", that carry" + car.passengers + ". It's " + car.color + " amaze many and it" + car.convertible + " with a mileage of" + car.mileage )
+
+// Exercise 5 , 1
+const Students = {
+    math: [76, 68, 86, 97, 55, 62, 71, 68, 75, 64, 98, 63, 87, 69, 73, 98, 69, 78, 96, 76]
+};
+
+// a 
+console.log(Students.math.length)
+
+// b
+
+function average(){
+    
+}
+
+// c
+
+
+if (80 > 90){
+    console.log( Students.length + " are students with D" )
+
+} 
+//else(41 > 60){
+ //   console.log( Students.length + " are students with C" )
+//}
+/*else if (61 > 80){
+    console.log( Students.length + " are students with B" )
+}*/
+    
+
